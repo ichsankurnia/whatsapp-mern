396c20f7162e5c5ffce3fc2a33535e950cd52c0b
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-09-15 20:53:15 +0700

    completed styling sidebar component

--- fe/src/Sidebar.js
+++ fe/src/Sidebar.js
@@ -4,10 +4,13 @@ import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge"
 import ChatIcon from "@material-ui/icons/Chat"
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar(){
     return (
         <div className="sidebar">
+            
             <div className="sidebar__header">
                 <Avatar src="https://ui-avatars.com/api/?name=Ichsan+Kurniawan&background=0D8ABC&color=fff"/>
                 <div className="sidebar__headerRight">
@@ -22,8 +25,18 @@ function Sidebar(){
                     </IconButton>
                 </div>
             </div>
-            <div className="sidebar__search">
 
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input type="text" placeholder="Search or start new chat" />
+                </div>
+            </div>
+
+            <div className="sidebar__chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
             </div>
         </div>
     )
