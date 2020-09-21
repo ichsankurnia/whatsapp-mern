48727f5e01af278181815f504e762fae37ec6b26
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-09-21 10:17:40 +0700

    mapping messages to component

--- fe/src/Chat.js
+++ fe/src/Chat.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Chat.css"
+import axios from "./axios"
+import moment from 'moment-timezone';   
 
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined, InsertEmoticon, Mic } from "@material-ui/icons";
 
-function Chat(){
+moment.tz.setDefault("Asia/Jakarta");
+
+function Chat({ messages }){                                        // props.messages
+    const [text, setText] = useState('')
+
+    useEffect(() => {
+        var objDiv = document.getElementById("chat__body");
+        objDiv.scrollTop = objDiv.scrollHeight;
+    })                                                              // jika tidak ada state yg terupdate, array kosong [] tidak perlu ditambahkan
+
+    const sendMessage = async (e) => {
+        e.preventDefault()
+
+        const payload = {
+            name: "ories",
+            message: text,
+            timestamp: moment(new Date()).format("dddd, DD-MM-YYYY hh:mm:ss A"),
+            received: true
+        }
+
+        if(text !== ''){
+            const res = await axios.post('messages/new', payload)
+
+            console.log(res)
+        }
+
+        setText('')
+    }
+
     return (
         <div className="chat">
             <div className="chat__header">
@@ -25,32 +55,22 @@ function Chat(){
                </div>
             </div>
 
-            <div className="chat__body">
-                <p className="chat__message">
-                    <span className="chat__name">Ories</span>
-                    This is a message
-                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
-                </p>
-
-                <p className="chat__message chat__receiver">
-                    <span className="chat__name">Ichsan</span>
-                    This is a message
-                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
-                </p>
-
-                <p className="chat__message">
-                    <span className="chat__name">Ories</span>
-                    This is a message
-                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
-                </p>
+            <div className="chat__body" id="chat__body">
+                {messages.map(({ name, message, timestamp, received }, key) => (                               // destructure object (data, key)
+                    <p className={`chat__message ${received && "chat__receiver"}`} key={key}>
+                        <span className="chat__name">{name}</span>
+                        {message}
+                        <span className="chat__timestamp">{timestamp}</span>
+                    </p>
+                ))}
             </div>
 
             <div className="chat__footer">
                 <InsertEmoticon />
                 <AttachFile />
                 <form>
-                    <input type="text" placeholder="Type a message" />
-                    <button type="submit" />
+                    <input type="text" placeholder="Type a message" value={text} onChange={(e) => setText(e.target.value)} />
+                    <button type="submit" onClick={sendMessage} />
                 </form>
                 <Mic />
             </div>
