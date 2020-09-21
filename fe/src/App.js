902e28d9746305b86131935af5ea78cecd1d00f1
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-09-21 10:18:23 +0700

    get all messages from api and pusher

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Pusher from "pusher-js";
+import axios from "./axios"
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 
 function App() {
-  return (
-    <div className="app">
-      <div className="app__body">
-        <Sidebar />
-        <Chat />
-      </div>
-    </div>
-  );
+	const [messages, setMessages] = useState([])
+
+	useEffect(() => {
+		axios.get("/messages/sync").then((res) => {
+			setMessages(res.data)
+		})
+	}, [])
+
+	useEffect(() => {
+		const pusher = new Pusher('4495e494430a0c853b2a', {
+			cluster: 'ap1'
+		});
+
+		const channel = pusher.subscribe('messages');
+			channel.bind('inserted', (data) => {
+			// alert(JSON.stringify(data));
+			setMessages([...messages, data])
+		});
+		
+		return () => {
+			channel.unbind_all()
+			channel.unsubscribe()
+		}
+    
+	}, [messages])                                  // cantumkan state yg terupdate dalam function useEffect ini
+
+	console.log(messages)
+
+	return (
+		<div className="app">
+			<div className="app__body">
+				<Sidebar />
+				<Chat messages={messages} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
