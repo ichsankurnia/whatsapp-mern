f962558fbeccd61b548aecf19f6a66bfb0d50e85
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-09-25 21:25:50 +0700

    add request register user

--- be/app/controllers/controller-user.js
+++ be/app/controllers/controller-user.js
@@ -1,4 +1,27 @@
 import User from "../../models/user.js"
+import { encryptAes } from "../helper/encrypt.js";
+
+
+const registerUser = async (req, res) => {
+    try {
+        const { username, password, email } = req.body
+        
+        const data = await User.create({
+            username: username,
+            password: await encryptAes(password),
+            email: email
+        })
+
+        if(data){
+            return res.status(201).json({code: 0, message: "succes register new user", data: data})
+        }else{
+            return res.status(500).json({code: 1, message: "fail register new user", data: null})
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({code: 1, message: error.message, data: null})
+    }
+}
 
 
 const createNewUser = async (req, res) => {
@@ -8,11 +31,11 @@ const createNewUser = async (req, res) => {
         const data = await User.create(reqPayload)
 
         if(data)
-            return res.status(201).json({code: 0, message: "success register new user", data: data})
+            return res.status(201).json({code: 0, message: "success add new user", data: data})
         else
             return res.status(500).json({code: 1, message: "fail register new user", data: null})
     } catch (error) {
-        return res.status(500).json({code: 1, message: error.message, data: null})
+        return res.status(400).json({code: 1, message: error.message, data: null})
     }
 }
 
@@ -43,4 +66,4 @@ const truncateUser = async (req, res) => {
 }
 
 
-export { createNewUser, getAllUser, truncateUser }
+export { registerUser, createNewUser, getAllUser, truncateUser }
