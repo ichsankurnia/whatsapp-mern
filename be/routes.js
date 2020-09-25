2294f4525c3a735db69de7a2f17974a747e48146
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-09-25 21:31:38 +0700

    add route user login and register

--- be/routes.js
+++ be/routes.js
@@ -1,14 +1,20 @@
 import express from "express";
 import { 
+    registerUser,
     createNewUser, 
     getAllUser, 
     truncateUser 
 } from "./app/controllers/controller-user.js";
-
+import login from "./app/middlewares/authenticate.js"
+import isAuthenticate from "./app/middlewares/verify-token.js"
 
 const Router = express.Router();
 
+
 Router.get('/', (req, res) => res.status(200).json({code: 0, message: "api running", author: "ichsankurnia"}))
+
+Router.post('/user/sign-up', registerUser)
+Router.post('/user/sign-in', login)
 
 Router.get('/user', getAllUser)
 Router.post('/user', createNewUser)
