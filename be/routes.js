fbc48e72bc5e9c6513a1b9270d2844cdaacd880f
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-09-26 22:51:08 +0700

    add route update profile

--- be/routes.js
+++ be/routes.js
@@ -6,24 +6,32 @@ import {
     getOneUser,
     updateUser,
     deleteUser,
-    truncateUser 
+    truncateUser ,
+    updateProfile
 } from "./app/controllers/controller-user.js";
 import login from "./app/middlewares/authenticate.js"
 import isAuthenticate from "./app/middlewares/verify-token.js"
 
+
 const Router = express.Router();
 
+Router.get('/', (req, res) => res.status(200).json({ author: 'Ichsan Kurniawan 😎', contact: 'kurniaichsan45@gmail.com', description: 'API Social Media chat application with NodeJS ExpressJS Pusher and MongoDB 🚀🚀🚀'}))
 
-Router.get('/', (req, res) => res.status(200).json({code: 0, message: "api running", author: "ichsankurnia"}))
-
+// Authentication
 Router.post('/user/sign-up', registerUser)
 Router.post('/user/sign-in', login)
 
+// User
 Router.get('/user', getAllUser)
 Router.get('/user/:id', getOneUser)
 Router.post('/user', createNewUser)
 Router.put('/user/:id', updateUser)
 Router.delete('/user/:id', deleteUser)
 Router.delete('/truncate-user', truncateUser)
+
+// Profile
+Router.put('/user/profile/:user_id', updateProfile)
+
+
 
 export default Router
