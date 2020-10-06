459c9def1154c856999ca92875a867fcedebc96e
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-10-06 15:28:51 +0700

    create model contact

---
+++ be/models/contact.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import moment from 'moment';   
+
+
+const contactSchema = mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "c_users"
+    },
+    contact: {
+        unique: true,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "c_users"
+    },
+    created_at: {
+        type: String,
+        default: moment(new Date()).format("dddd, DD-MM-YYYY hh:mm:ss A") 
+    },
+    updated_at: { 
+        type: String, 
+        default: moment(new Date()).format("dddd, DD-MM-YYYY hh:mm:ss A") 
+    }
+})
+
+
+// Collection
+export default mongoose.model('c_contacts', contactSchema)
