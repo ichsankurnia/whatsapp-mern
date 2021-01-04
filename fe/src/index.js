0d580887f64f20a7ac9326a8104f8d52ec2ce539
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-01-04 18:10:26 +0700

    change main to login as root component

--- fe/src/index.js
+++ fe/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Main from './Main';
+import Login from './Login';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
