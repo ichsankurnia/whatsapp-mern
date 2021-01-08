b512d0f3ecbeb555ae75bb9bcc7e232aaf889e59
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-01-08 18:08:07 +0700

    fixing model fail to import

--- be/app/controllers/controller-message.js
+++ be/app/controllers/controller-message.js
@@ -1,5 +1,5 @@
-import Message from "../../models/message";
-import User from "../../models/user";
+import Message from "../../models/message.js";
+import User from "../../models/user.js";
 
 
 const newMessage = async (req, res) => {
