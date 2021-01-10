85b9966b67f8657ff6ea51da69c86d85843d72d6
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-01-10 20:36:20 +0700

    Create rootReducer.js

---
+++ fe/src/redux/reducer/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import globalReducer from "./globalReducer";
+
+const rootReducer = combineReducers({
+    global: globalReducer
+})
+
+export default rootReducer
+
+// Action => reducer => updateStore/state
