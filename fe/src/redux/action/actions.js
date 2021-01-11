fada18e16b4f546555bb116ff6928fad7b6698c4
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-01-11 17:18:10 +0700

    add action to show/unshow contact component

--- fe/src/redux/action/actions.js
+++ fe/src/redux/action/actions.js
@@ -1,6 +1,7 @@
 export const ActionType = {
     CHAT_ON: "CHAT_ON",
-    CHAT_OFF: "CHAT_OFF"
+    CHAT_OFF: "CHAT_OFF",
+    CONTACT_ONOFF: "CONTACT_ONOFF"
 }
 
 export const setChatOn = (payload) => {
@@ -14,5 +15,12 @@ export const setChatOff = (payload) => {
     return {
         type: ActionType.CHAT_OFF,
         payload: payload
+    }
+}
+
+export const setContactOnOff = (status) => {
+    return {
+        type: ActionType.CONTACT_ONOFF,
+        status: status
     }
 }
