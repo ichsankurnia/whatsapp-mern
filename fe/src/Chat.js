a7cff992baf8aa5d8fea352780a94318fd0559ac
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-01-11 17:20:30 +0700

    add redux to chat component

--- fe/src/Chat.js
+++ fe/src/Chat.js
@@ -7,9 +7,15 @@ import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined, InsertEmoticon, Mic } from "@material-ui/icons";
 import socket from "./socket";
 
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { setChatOn } from "./redux/action/actions";
+
+import waOverview from './assets/wa.png'
+
 moment.tz.setDefault("Asia/Jakarta");
 
-function Chat({ messages }){                                        // props.messages
+function Chat({ messages, globalState }){                                        // props.messages
     const [text, setText] = useState('')
 
     const sendMessage = async (e) => {
@@ -33,6 +39,8 @@ function Chat({ messages }){                                        // props.mes
     }
 
     return (
+        <>
+        {globalState.chatOn?
         <div className="chat">
             <div className="chat__header">
                <Avatar />
@@ -73,7 +81,24 @@ function Chat({ messages }){                                        // props.mes
             </div>
 
         </div>
+        :
+        <div className="chat">
+            <img src={waOverview} alt="overview" height='100%' />
+        </div>
+        }
+        </>
+
     )
 }
 
-export default Chat
+const mapStateToProps = (state) => {
+    return {
+        globalState: state.global
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({setChatOn}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Chat)
