4983d5fc2538db7704d806c5399eea72e1029201
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-01-11 17:21:29 +0700

    add authentication process

--- fe/src/Login.js
+++ fe/src/Login.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import App from './App'
 import axios from './axios'
 import './Login.css'
 
 function Login(){
+    const [app, setApp] = useState(false)
     const [register, setRegister] = useState(false)
     const [email, setEmail] = useState('')
     const [usn, setUsn] = useState('')
     const [pwd, setPwd] = useState('')
+
+
+    useEffect(() => {
+        if(localStorage.getItem('token')) setApp(true)
+        else setApp(false)
+    }, [])
+
 
     const moveToSignUp = () => {
         setRegister(!register)
@@ -27,9 +36,10 @@ function Login(){
             axios.post('api/v1/user/sign-up', payload).then((res) => {
                 console.log(res)
                 alert(res.data.message)
-                setRegister(false)
+                moveToSignUp()
             }).catch((err) => {
-                console.log(err.response)
+                if(err.response) alert(err.response.data.message)
+                else alert(JSON.parse(JSON.stringify(err)).message)
             })
         }else{
             const payload = {
@@ -37,60 +47,71 @@ function Login(){
                 password: pwd
             }
 
-            axios.post('api/v1/user/sign-in', payload).then((res) => {
+            try {
+                const res = await axios.post('api/v1/user/sign-in', payload)    
                 console.log(res)
+                localStorage.setItem('user', JSON.stringify(res.data.data))
                 localStorage.setItem('token', res.data.token)
-            }).catch((err) => {
-                console.log(err.response)
-            })
+                setApp(true)
+            } catch (error) {
+                if(error.response) alert(error.response.data.message)
+                else alert(JSON.parse(JSON.stringify(error)).message)
+            }
         }
     }
     
     return (
-        <div id="card">
-            <div id="card-content">
-                <div id="card-title">
-                    <h2>{register? "SIGN UP" : "SIGN IN"}</h2>
-                    <div className="underline-title"></div>
+        <>
+        {app? 
+            <App />
+            :
+            <div id="card">
+                <div id="card-content">
+                    <div id="card-title">
+                        <h2>{register? "SIGN UP" : "SIGN IN"}</h2>
+                        <div className="underline-title"></div>
+                    </div>
+                    <form className="form">
+                        {register &&
+                        <>
+                            <label htmlFor="user-email" style={{marginTop: 22}}>
+                                &nbsp;Email
+                            </label>
+                            <input id="user-email" className="form-content" type="email" name="email" onChange={(e) => setEmail(e.target.value)} value={email} autoComplete='true' required />
+                            <div className="form-border"></div>
+                            <div className="form-border"></div>
+                            <label htmlFor="user-name" style={{paddingTop: 22}}>
+                                &nbsp;Username
+                            </label>
+                            <input id="user-name" className="form-content" type="text" name="text" onChange={(e) => setUsn(e.target.value)} value={usn} required />
+                            <div className="form-border"></div>
+                            <div className="form-border"></div>
+                        </>
+                        }
+                        {!register && 
+                        <>
+                            <label htmlFor="user-email" style={{marginTop: 22}}>
+                                &nbsp;Username / Email
+                            </label>
+                            <input id="user-email" className="form-content" type="text" name="email" onChange={(e) => setUsn(e.target.value)} value={usn} autoComplete='true' required />
+                            <div className="form-border"></div>
+                            <div className="form-border"></div>
+                        </>
+                        }
+                        <label htmlFor="user-password" style={{paddingTop: 22}}>
+                            &nbsp;Password
+                        </label>
+                        <input id="user-password" className="form-content" type="password" name="password" onChange={(e) => setPwd(e.target.value)} value={pwd} required />
+                        <div className="form-border"></div>
+                        <div className="form-border"></div>
+                        <input id="submit-btn" type="submit" name="submit" value={register? "SIGN UP" : "SIGN IN"} onClick={handleSubmitForm} />
+                        <p id="signup" style={{cursor: 'pointer'}} onClick={moveToSignUp} >{register? "Already have an account" : "Don't have account yet"}</p>
+                    </form>
                 </div>
-                <form className="form">
-                    {register &&
-                    <>
-                        <label htmlFor="user-email" style={{marginTop: 22}}>
-                            &nbsp;Email
-                        </label>
-                        <input id="user-email" className="form-content" type="email" name="email" onChange={(e) => setEmail(e.target.value)} value={email} autoComplete='true' required />
-                        <div className="form-border"></div>
-                        <div className="form-border"></div>
-                        <label htmlFor="user-name" style={{paddingTop: 22}}>
-                            &nbsp;Username
-                        </label>
-                        <input id="user-name" className="form-content" type="text" name="text" onChange={(e) => setUsn(e.target.value)} value={usn} required />
-                        <div className="form-border"></div>
-                        <div className="form-border"></div>
-                    </>
-                    }
-                    {!register && 
-                    <>
-                        <label htmlFor="user-email" style={{marginTop: 22}}>
-                            &nbsp;Username / Email
-                        </label>
-                        <input id="user-email" className="form-content" type="text" name="email" onChange={(e) => setUsn(e.target.value)} value={usn} autoComplete='true' required />
-                        <div className="form-border"></div>
-                        <div className="form-border"></div>
-                    </>
-                    }
-                    <label htmlFor="user-password" style={{paddingTop: 22}}>
-                        &nbsp;Password
-                    </label>
-                    <input id="user-password" className="form-content" type="password" name="password" onChange={(e) => setPwd(e.target.value)} value={pwd} required />
-                    <div className="form-border"></div>
-                    <div className="form-border"></div>
-                    <input id="submit-btn" type="submit" name="submit" value={register? "SIGN UP" : "SIGN IN"} onClick={handleSubmitForm} />
-                    <p id="signup" style={{cursor: 'pointer'}} onClick={moveToSignUp} >{register? "Already have an account" : "Don't have account yet"}</p>
-                </form>
             </div>
-        </div>
+
+        }
+        </>
     )
 }
 
