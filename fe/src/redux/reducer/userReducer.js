5af15ce18a0d0d42752a77c4c8188839f8cb624f
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-02-16 11:30:38 +0700

    Create userReducer.js

---
+++ fe/src/redux/reducer/userReducer.js
@@ -0,0 +1,32 @@
+import initialState from './intialState'
+import { ActionType } from '../action/actions'
+
+const userReducer = (state = initialState.user, action) => {
+    console.log(action)
+    switch (action.type){
+        case ActionType.SET_CONTACT_LIST:
+            return {
+                ...state,
+                contact_list: action.data
+            }
+        case ActionType.SET_CONVERSATION_LIST:
+            return {
+                ...state,
+                conversation_list: action.data
+            }
+        case ActionType.SET_USER_ID:
+            return {
+                ...state,
+                user_id: action.id
+            }
+        case ActionType.SET_PHONE_NUMBER:
+            return {
+                ...state,
+                phone_number: action.phone_number
+            }
+        default:
+            return state
+    }
+}
+
+export default userReducer
