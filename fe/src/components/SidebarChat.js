bd7a14fb20f74c6e37d066e9fded236388724e62
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-03-10 11:18:58 +0700

    fixing error show conversation list map when recipients is null

--- fe/src/components/SidebarChat.js
+++ fe/src/components/SidebarChat.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import "./SidebarChat.css"
 
 import { Avatar } from "@material-ui/core"
@@ -6,24 +6,13 @@ import { connect } from "react-redux"
 
 
 function SidebarChat({selected, onClick, conversation, userState}){
-    // const [listConvBasedContact, setListConvBasedContact] = useState([])
-    // const [name, setName] = useState('')
-
-    useEffect(() => {
-        // conversation?.recipients.forEach(phone_number => {
-        console.log(userState.contact_list.find(contacts => contacts.contact.phone_number === '6281234567891')?.contact.username)
-        // });
-    })
-
-    console.log(userState.contact_list)
-
     
     return(
         <div className={selected? "sidebarChat-active" : "sidebarChat"} onClick={onClick}>
             <Avatar />
             <div className="sidebarChat__info">
                 {
-                    conversation?.recipients.map((data, index) => (
+                    conversation?.recipients?.map((data, index) => (
                         <h2 key={index}>
                             {/* <span>{userState.contact_list.find(contacts => contacts.contact.phone_number === data)?.contact.username}</span> */}
                             {userState.contact_list.find(contacts => contacts.contact.phone_number === data)? 
