39ad573600cf12ceb62451fdcef59881ace1d3f9
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-03-13 02:34:22 +0700

    add group collection

---
+++ be/models/group.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import moment from 'moment';   
+
+const groupSchema = mongoose.Schema({
+    group_id: {
+        type: String,
+        unique: true,
+        required: [true, 'group id is required']
+    },
+    group_name: {
+        type: String,
+        unique: true,
+        required: [true, 'group name is required']
+    },
+    group_desc: {
+        type: String,
+    },
+    group_photo: {
+        type: String,
+    },
+    group_member: [{
+        type: String,
+        unique: true,
+        required: [true, 'phone number is required']
+    }],
+    group_maker: {
+        type: String,
+    },
+    created_at: {
+        type: String,
+        default: moment(new Date()).format("dddd, DD-MM-YYYY hh:mm:ss A") 
+    },
+    updated_at: { 
+        type: String, 
+        default: moment(new Date()).format("dddd, DD-MM-YYYY hh:mm:ss A") 
+    }
+})
+
+
+// Collection
+export default mongoose.model('c_groups', groupSchema)
