00d99068361e66db12f8278ba5039befb0f05857
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-03-13 23:02:41 +0700

    fixing delete group

--- be/app/controllers/controller-group.js
+++ be/app/controllers/controller-group.js
@@ -3,12 +3,17 @@ import Group from "../../models/group.js";
 import User from "../../models/user.js";
 import { idPhoneNumber } from "../helper/helper.js";
 
+function checkIfDuplicateExists(w){
+    return new Set(w).size !== w.length 
+}
 
 const addNewGroup = async (req, res) => {
     try {
         const { group_member } = req.body
 
         if(group_member.length === 0) return res.status(400).json({code: 1, message: `group member is empty ☹️`, data: null})
+        
+        if(checkIfDuplicateExists(group_member)) return res.status(400).json({code: 1, message: `group member contains duplicate number ☹️`, data: null})
 
         // Check wheter phone number is existed
         await Promise.all(group_member.map(async (phoneNumber) => {
@@ -16,8 +21,8 @@ const addNewGroup = async (req, res) => {
 
             if(!findUser) return res.status(400).json({code: 1, message: `group member with phone_number ${phoneNumber} doesn't exist ☹️`, data: null})
         }))
-
-        // Create group
+        
+        // // Create group
         const data = await Group.create(req.body)
 
         if(data){
@@ -50,39 +55,43 @@ const deleteGroup = async (req, res) => {
     try {
         const { id } = req.params
 
-        console.log(id)
-
         var ObjectId = mongoose.Types.ObjectId;
         var objId = new ObjectId( (id.length < 12) ? "123456789012" : id );
 
         const findGroup = await Group.findOne({ 
             $or: [ { _id: objId }, { group_id: id }, ]
-        }).or()
+        })
 
-        console.log(findGroup)
         if(!findGroup) return res.status(400).json({code: 1, message: `failed delete group, group with id '${id} doesn't exist ☹️`, data: null})
 
-        // const data = await Group.findOneAndDelete(
-        //     { $or:
-        //         [ { _id: id }, { group_id: id } ]
-        //     }
-        // )
+        const deleteGroup = await Group.findOneAndDelete(
+            { $or:
+                [ { _id: objId }, { group_id: id } ]
+            }
+        )
 
-        // if(deleteGroup) {
-        //     await User.updateOne(
-        //         { _id: user_id },
-        //         {
-        //             $pull : { groups: data._id }
-        //         }
-        //     )
-        // }
+        if(deleteGroup) {
+            await Promise.all(findGroup.group_member.map(async (phoneNumber) => {
+                await User.updateOne(
+                    {
+                        phone_number: idPhoneNumber(phoneNumber)
+                    },
+                    {
+                        $pull : { groups: deleteGroup._id }
+                    }
+                )
+            }))
+        }else{
+            return res.status(500).json({code: 1, message: `delete group failed ☹️`, data: null})
+        }
 
-        return res.status(200).json({code: 0, message: 'success'})
+        return res.status(200).json({code: 0, message: 'success delete group', data: deleteGroup})
     } catch (error) {
         console.log(error)
         return res.status(400).json({code: 1, message: `${error.message} ☹️`, data: null})
     }
 }
+
 
 export {
     addNewGroup,
