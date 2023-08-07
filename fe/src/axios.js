83eccd29a8e9b6f0693d6c76c50fc268c891958a
Ichsan Kurniawan <ichsankurniawan51@gmail.com>
2023-08-07 23:32:05 +0700

    update

--- fe/src/axios.js
+++ fe/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const baseURL = "http://localhost:9000"
-export const baseURL = 'https://heroku-node-whatasappbe.herokuapp.com'
+export const baseURL = "http://localhost:9000"
+// export const baseURL = 'https://heroku-node-whatasappbe.herokuapp.com'
 
 const instance = axios.create({
     baseURL: baseURL
